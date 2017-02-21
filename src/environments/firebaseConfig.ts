8c1d0ece550dfbce96e51675c43197207f6e339b
Claudiu Farcas <farcas.claudiu@gmail.com>
2017-02-21 05:46:08 +0200

    mm

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -5,11 +5,11 @@ import {AuthMethods, AuthProviders} from "angularfire2";
 export const firebaseConfig = {
     //get these from your created firebase project at https://console.firebase.google.com
     // Paste all this from the Firebase console...
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "***REMOVED***",
+    authDomain: "***REMOVED***",
+    databaseURL: "***REMOVED***",
+    storageBucket: "***REMOVED***",
+    messagingSenderId: "***REMOVED***"
 };
 
 export const authConfig = {
